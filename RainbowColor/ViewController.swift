833ff3a57602d26bcba24c41068a575fca00ae04
Liseth Cardozo <cardozoliseth@gmail.com>
2017-09-11 15:20:00 -0700

    Initial Commit

--- RainbowColor/ViewController.swift
+++ RainbowColor/ViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -22,4 +26,32 @@ class ViewController: UIViewController {
 
 
 }
+
+extension ViewController: UITableViewDataSource {
+    // How many cells are we going to need?
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+        // return an integer that indicates how many rows (cells) to draw
+    }
+    
+    // How should I create each cell?
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // Get the UITableViewCell and create/populate it with data then return it
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        let colors = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple]
+        //set the cell height
+        tableView.rowHeight = 120
+        
+        //set cell color
+        cell.backgroundColor = colors[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}
+
+
+
 
